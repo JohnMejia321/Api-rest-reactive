0aa9c5ee7ba008902e646dd12dd9fc29eef7a540
JohnMejia <john.mejias@udea.edu.co>
2023-06-26 19:13:43 -0500

    first commit

---
+++ src/main/java/com/example/ApiRestReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestReactiveApplication.class, args);
+	}
+
+}
